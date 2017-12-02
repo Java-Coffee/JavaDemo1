a7c1f21f621e81c66e70a5b0fbe6cfb262294e4a
javacoffee <1095908464@qq>
2017-12-02 23:58:32 +0800

    算法 day4

--- javalib/src/main/java/com/example/logic_day4.java
+++ javalib/src/main/java/com/example/logic_day4.java
@@ -3,7 +3,9 @@ import java.util.Arrays;
 public class logic_day4 {
     public static void main(String[] args) {
         int[] input = {2,6,7,4,3,2,1,5,8,4};
-        System.out.println(Arrays.toString(xuanze(input)));
+        System.out.println("选择排序"+Arrays.toString(xuanze(input)));
+        System.out.println("冒泡排序"+Arrays.toString(maopao(input)));
+        System.out.println("插入排序"+Arrays.toString(insertSort(input)));
 
     }
 
@@ -20,6 +22,37 @@ public class logic_day4 {
                     s[i] = temp;
                 }
             }
+        }
+        return s;
+    }
+
+    public static int[] maopao(int[] s){
+        int temp = 0;
+        int len = s.length;
+        for(int i = 0;i<len;i++){
+            for(int j = i;j<len-i-1;j++){
+                if(s[j] > s[j+1]){
+                    temp = s[j];
+                    s[j] = s[j+1];
+                    s[j+1] = temp;
+                }
+            }
+        }
+        return s;
+    }
+
+    public static int[] insertSort(int[] s){
+        int index = 0;//要插入的位置
+        int temp = 0;
+        for(int i = 1;i<s.length;i++){
+            temp = s[i];
+            index = i;  //初始要插入的位置
+            for (int j = i-1;j>= 0;j--){
+                if(s[j]<temp)break;
+                index = j;
+                s[j+1] = s[j];
+            }
+            s[index] = temp;
         }
         return s;
     }
