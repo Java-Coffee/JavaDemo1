6c1361850739a615804efcf456663ab95c7f0f21
javacoffee <1095908464@qq>
2017-12-02 22:42:42 +0800

    算法 day3

--- javalib/src/main/java/com/example/logic_day3.java
+++ javalib/src/main/java/com/example/logic_day3.java
@@ -11,15 +11,17 @@
 //
 //        Output: “bb”
 
-import java.util.HashMap;
-import java.util.Map;
+
 
 public class logic_day3 {
     public static void main(String[] args) {
-        String s = getLongestSubString("abcbadac");
-        System.out.println("s = "+s);
+        //String s = getLongestSubString("abcbadac");
+        //System.out.println("s = "+s);
+        int len = getLongestSubString1("abcbadac");
+        System.out.println("len = "+len);
     }
 
+    //判断当前字母的左右是否相等
     public static String getLongestSubString(String s){
         int slen = s.length();
         int indexS = 0;
@@ -27,6 +29,8 @@ public class logic_day3 {
         int indexD = 0;
         int beginD = 0;
         for(int i = 0;i < slen;i++){
+
+            //回文子串是偶数的情况
             int left = i-1,right = i;
             while (left >= 0 &&right <slen&&(s.charAt(left) == s.charAt(right))){
                 left--;
@@ -38,6 +42,7 @@ public class logic_day3 {
                 beginS = left+1;
             }
 
+            //回文子串是奇数的情况
             left = i-1;right = i+1;
             while (left >= 0 &&right <slen&&(s.charAt(left) == s.charAt(right))){
                 left--;
@@ -51,5 +56,22 @@ public class logic_day3 {
 
         }
         return beginS>beginD?s.substring(beginS,beginS+indexS):s.substring(beginD,beginD+indexD);
+    }
+
+    public static int getLongestSubString1(String s){
+        String subS = "";
+        String t = "cadabcba";
+        int len = s.length();
+        int longest = 0;
+        for(int i = 0;i<len-1;i++){
+            for(int j = len-1;j>i;j--){
+                subS = t.substring(i,j);
+                if(s.contains(subS)){
+                    System.out.println("subS = "+subS);
+                    longest = Math.max(longest,j-i+1);
+                }
+            }
+        }
+        return longest;
     }
 }
